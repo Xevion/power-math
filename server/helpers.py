6fdfa5ef9463672d32fd123162dab4c43c6b281e
Xevion <xevioni@yandex.com>
2020-12-15 10:43:26 -0600

    repository reformat (remove python)

--- server/helpers.py
+++
@@ -1,6 +0,0 @@
-import random
-import string
-
-
-def generate_id(length: int):
-    return ''.join(random.choices(string.ascii_letters, k=length))
--- server/questions.py
+++
@@ -1,6 +0,0 @@
-import random
-from server import arithmetic
-
-
-def get_arithmetic():
-    return random.choice(arithmetic.questions)
